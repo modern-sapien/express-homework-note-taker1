e5329ab06fc176f008fe20d0888426e5a1f840bb
Jonathan Canales <joncanales2@gmail.com>
2020-10-01 18:29:27 -0400

    GET & POST for API works, working on HTML functionality now.

--- server.js
+++ server.js
@@ -10,22 +10,51 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// GET ROUTES
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err,data) =>  {
+        if(err){
+            console.log(err);
+            return res.status(500).json({
+                error: true,
+                data: null,
+                message: "unable to retrieve notes."
+            });
+        }
+        res.json({
+            error: false,
+            data: JSON.parse(data),
+            message: "successfully retrieved notes.",
+        });
+    })
+})
 
-
-// POST ROUTES
+// POST ROUTES - NOTES
 app.post("/api/notes", (req, res) =>    {
     console.log(req.body);
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if(err) throw err;
+        if(err) {
+            console.log(err);
+            return res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to add a new note.",
+            }) 
+        };
         console.log(data);
         const updatedData = JSON.parse(data);
         updatedData.push(req.body);
         console.log(updatedData);
-        fs.writeFile("./db/db.json", JSON.stringify(updatedData), (err)   =>  {
+        fs.writeFile("./db/db.json", JSON.stringify(updatedData, null, 2), (err)   =>  {
             if (err) throw err;
+            res.json({
+                error: false,
+                data: null,
+                message: "Successfully added new note."
+            });
         })
     })
-})
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT:" + PORT);
