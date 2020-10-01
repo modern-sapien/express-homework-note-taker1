c79ca11600892ecf34bb706abb53bd2147687e39
Jonathan Canales <joncanales2@gmail.com>
2020-10-01 16:40:29 -0400

    post route working with console.log

--- server.js
+++ server.js
@@ -1,38 +1,26 @@
 const express = require("express");
 const path = require("path");
-
-const PORT = 8080;
+const fs = require("fs");
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
+// POST ROUTES
+app.post("/api/notes", (req, res) =>    {
+    console.log(req.body);
+})
+
 app.listen(PORT, function() {
-    console.log("App listening on PORT" + PORT);
+    console.log("App listening on PORT:" + PORT);
 })
 
-
-// s
-app.get("/notes", function(req, res)    {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-});
-
-app.get("/api/notes", function(req, res)    {
-    res.json()
-});
+// GET ROUTES
 
 
-// POST creating a note
-app.post("/api/notes", function(req, res)   {
-    const notes = fs.writeFile(JSON.stringify(req.body));
-    res.json(notes);
-})
-
-// DELETE for deleting previously saved notes
-
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
