ea17552668cafca4a7fba3f6c8241798da08a32e
Jonathan Canales <joncanales2@gmail.com>
2020-10-01 22:33:01 -0400

    struggled through this one.

--- server.js
+++ server.js
@@ -2,59 +2,53 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const PORT = process.env.PORT || 8080;
-
+const fakeDataBase = "./db/db.json"
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join("./public")));
 
 //API GET ROUTES
-app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err,data) =>  {
-        if(err){
-            console.log(err);
-            return res.status(500).json({
-                error: true,
-                data: null,
-                message: "unable to retrieve notes."
-            });
-        }
-        res.json({
-            error: false,
-            data: JSON.parse(data),
-            message: "successfully retrieved notes.",
-        });
-    })
-})
+app.get("/api/notes", function(req, res){
+    return res.sendFile(path.join(__dirname, "db/db.json"))
+});
 
 // API POST ROUTES - NOTES
 app.post("/api/notes", (req, res) =>    {
     console.log(req.body);
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    // if something in db/db.json this
+    fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log(err);
+        return res.json({
+            error: true,
+            data: null,
+            message: "Unable to get note."
+        });
+    }
+    const updatedData = JSON.parse(data);
+    req.body.id = updatedData.length;
+    updatedData.push(req.body);
+    console.log(updatedData);
+    fs.writeFile("db/db.json", JSON.stringify(updatedData), (err) =>  {
         if(err) {
             console.log(err);
-            return res.status(500).json({
+            return res.json({
                 error: true,
                 data: null,
-                message: "Unable to add a new note.",
-            }) 
-        };
-        console.log(data);
-        const updatedData = JSON.parse(data);
-        updatedData.push(req.body);
-        console.log(updatedData);
-        fs.writeFile("./db/db.json", JSON.stringify(updatedData, null, 2), (err)   =>  {
-            if (err) throw err;
-            res.json({
-                error: false,
-                data: null,
-                message: "Successfully added new note."
+                message: "Unable to save note.",
             });
-        })
+        }
+        res.json({
+            error: false,
+            data: updatedData,
+            message: "successfully added new note.",
+        });
+     })
     })
-});
+})
 
 // GET ROUTE HTML
 app.get("/", function(req, res) {
@@ -63,22 +57,8 @@ app.get("/", function(req, res) {
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
-    res.body(data);
 });
 
-// POST ROUTE HTML
-app.post("/notes", function(req, res)   {
-    const notes = fs.readFile("./db/db.json", "utf-8", (err, data) =>{
-        if(err) {
-            console.log(err)
-        }
-    if(data) {
-        res.body(data)
-        console.log("there should be new data here" + data);
-    }
-    })
-
-})
 
 
 
